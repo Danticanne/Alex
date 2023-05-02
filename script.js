6b0ce333370cb94069c6269d82012c0a4abaa4ff
Antoine H <ddanticanne@gmail.com>
2023-05-02 17:14:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function heho(){
+    document.getElementById('test').play()
+    document.getElementById('img').style.opacity = 1
+    document.getElementById('emoji').style.opacity = 1
+    document.getElementById('bg').style.animation = 'rainbow 2s infinite'
+    document.getElementById('title').style.opacity = 1
+}
